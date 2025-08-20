ba5c7cdac8224ab9993b46bf47a33b21f4618be6
Emmanuel Owiredu <emmanuelowiredu093@gmail.com>
2025-08-20 01:51:56 +0000

    Fix __dirname for ESM build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
+import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+
+// ESM replacement for __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  root: path.resolve(__dirname, "client"), // Root directory
   plugins: [
     react(),
+    runtimeErrorOverlay(),
+    ...(process.env.NODE_ENV !== "production" && process.env.REPL_ID
+      ? [
+          await import("@replit/vite-plugin-cartographer").then((m) =>
+            m.cartographer()
+          ),
+        ]
+      : []),
   ],
   resolve: {
     alias: {
@@ -13,15 +28,14 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
-    emptyOutDir: true,
+    outDir: path.resolve(__dirname, "dist/public"), // Build output
+    emptyOutDir: true, // Clear output folder before build
   },
   server: {
     fs: {
       strict: true,
-      deny: ["**/.*"],
+      deny: ["**/.*"], // Deny access to hidden files
     },
   },
 });
